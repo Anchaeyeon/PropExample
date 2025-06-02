628d46d71cedf079ab67f1a61ae645d4dcd80407
Chaeyeon Ahn <s2328@e-mirim.hs.kr>
2025-06-02 14:37:43 +0900

    const 상수, readonly 읽기전용변수 키워드 예제

--- Program.cs
+++ Program.cs
@@ -28,11 +28,24 @@ namespace PropExample
 
             // 정적 생성자 패턴 2
             Console.WriteLine("첫 번째 위치");
-            Sample sample2 = new Sample();
+            Sample sample2 = new Sample(); // Sample 객체 생성 전, 레퍼런스 변수 생성 전에 정적 생성자 호출
             Console.WriteLine("두번째 위치");
-            Console.WriteLine(Sample.value); // Sample 객체 생성 전, 레퍼런스 변수 생성 전에 정적 생성자 호출
+            Console.WriteLine(Sample.value);
             Console.WriteLine("세번째 위치");
 
+
+            //const vs readonly
+            Item item1 = new Item("고구마", 1500);
+            Item item2 = new Item("감자", 1000);
+            Item item3 = new Item("옥수수", 2000);
+            Item item4 = new Item("토란", 2500);
+            Console.WriteLine(item1.id);
+            //item1.id = 11;
+            Console.WriteLine(item2.id);
+            Console.WriteLine(item3.id);
+            Console.WriteLine(item4.id);
+
+            Console.WriteLine(Item.ApplicationName);
         }
 
         // 오버로딩
